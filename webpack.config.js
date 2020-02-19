cd8bd10a67fd78fb207e2bfb2dba0494d674305a
Konrad Rosa <40122362+kerekatu@users.noreply.github.com>
2020-02-19 11:23:55 +0100

    finished current webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path') // ES5 import syntax
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    // __dirname targets root path of the project
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+}
+
+// loader
